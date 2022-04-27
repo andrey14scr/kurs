55874f9fee53689c329d314b53574aa37d9848f4
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2022-04-27 13:44:47 +0300

    Data updated.

--- nn.py
+++ nn.py
@@ -18,15 +18,16 @@ def getLayers(input_size):
     ]
 
 # Adam parameters:
-learning_rate = 0.1
+learning_rate = 0.01
 alpha = 0.05
 beta = 0.05
 # Dropout parameters:
 dropout_step = 0.1
 # NN parameters:
-iterations_count = 12
-epochs_list = [1]
-batch_size_list = [1000]
+hidden_layer_size = 40
+iterations_count = 20
+epochs_list = [2]
+batch_size_list = [400]
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -44,20 +45,20 @@ test_data /= 255.0
 train_labels_cat = keras.utils.to_categorical(y_train, NUM_CLASSES)
 test_labels_cat = keras.utils.to_categorical(y_test, NUM_CLASSES)
 
+results = []
 dropout = 0
 for epochs in epochs_list:
     for batch_size in batch_size_list:
         f = open("nn.txt", "a")
-        f.write(f'\n    Epochs: {epochs}, Batch size: {batch_size}, Iterations: {iterations_count}\n\n')
+        f.write(f'\n    Epochs: {epochs}, Batch size: {batch_size}, Iterations: {iterations_count} ({hidden_layer_size})\n\n')
         f.close()
         while(dropout < 0.91):
-            res = []
+            tmp = []
             times = []
             for j in range(iterations_count):
                 model = Sequential()
-                for layer in getLayers(size):
-                    model.add(layer)
-                    model.add(Dropout(dropout))
+                model.add(Dense(hidden_layer_size, activation='relu', input_shape=(size,)))
+                model.add(Dropout(dropout))
                 model.add(Dense(NUM_CLASSES, activation='softmax'))
 
                 model.compile(loss=keras.losses.categorical_crossentropy,
@@ -77,12 +78,21 @@ for epochs in epochs_list:
                                  verbose=0,
                                  validation_data=(test_data, test_labels_cat))
                 toc = time.perf_counter()
-                times.append(toc - tic)
+                times.append((toc - tic) * 1000)
                 score = model.evaluate(test_data, test_labels_cat, verbose=0)
-                res.append(score[1])
+                tmp.append(score[1])
+            results.append({
+                "dp": round(dropout * 100, 1),
+                "acc": ave(tmp) * 100,
+                "time": ave(times)
+            })
             f = open("nn.txt", "a")
-            f.write(f'Dropout: {(dropout * 100):4.{1}f}%, Accuracy: {ave(res):.{6}f}, Min: {min(res):.{6}f}, Max: {max(res):.{6}f} Time: {ave(times):.{6}f}\n')
+            f.write(f'Dropout: {results[-1]["dp"]:4.{1}f}%, Accuracy: {results[-1]["acc"]:.{3}f}%, Min: {(min(tmp)*100):.{2}f}%, Max: {(max(tmp)*100):.{2}f}%, Time: {results[-1]["time"]:.{3}f}ms\n')
             f.close()
             dropout += dropout_step
 
-#model.save('mnist.h5')
+f = open("nn.txt", "a")
+f.write('\nFor excel:\n')
+for r in results:
+    f.write(f'{r["dp"]:4.{1}f}\t{r["acc"]:.{3}f}\t{r["time"]:.{3}f}\n')
+f.close()
